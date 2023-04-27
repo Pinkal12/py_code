6fb85a5b382a7f4a9d3335bf0e8018094bb2273d
Pinkal12 <pinkalsanoria123@gmail.com>
2023-04-27 17:56:50 +0530

    downlod excle file form database test

--- plot_project/plotapp/admin.py
+++ plot_project/plotapp/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Anomaly
 
 @admin.register(Anomaly)
+# seclet cim you want
 class AnomalyAdmin(admin.ModelAdmin):
     list_display = ('anomaly_number', 'anomaly_type', 'lat', 'lng', 'length', 'width', 'area', 'size', 'distance', 'location')
